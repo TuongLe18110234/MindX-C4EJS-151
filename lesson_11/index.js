a5f4240c3596bd81b1cf544b4595ebcf6c7f1664
TuongLN <tuongln@gmail.com>
2024-06-11 17:47:52 +0700

    Lesson 11: DOM, Events & Browser

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -1,3 +1,5 @@
+console.log(document);
+
 let trangChu = document.getElementById('trang-chu');
 
 console.log(trangChu);
@@ -5,7 +7,7 @@ trangChu.innerHTML = 'Trang chu moi';
 trangChu.style.color = 'pink';
 trangChu.style.cursor = 'pointer';
 
-let text = document.getElementById('text');
+let text = document.getElementById('text'); // Object thể hiện cho thẻ input
 text.placeholder = 'Something'
 text.type = 'password';
 
@@ -16,13 +18,95 @@ console.log(document.getElementsByTagName('p'));
 // document.querySelector()
 
 // Thêm mới hoặc xoá elements
-const todoList = ['Homework', 'Eat', 'Phay video game'];
-const ul = document.createElement('ul'); // Tạo mới một element
+const todoList = ['Homework', 'Eat', 'Play video game', 'Play soccer'];
+function addTodoList(search) {
+    console.log(search);
+    let filterList = [];
+    if (search == '') {
+        filterList = todoList;
+    } else {
+        filterList = todoList.filter(todo => todo.includes(search));
+        console.log(filterList);
+    }
+    const ul = document.createElement('ul'); // Tạo mới một element
 
-for (let i=0; i<todoList.length; i++) {
-    const li = document.createElement('li');
-    li.innerHTML = todoList[i];
-    ul.appendChild(li); // Thêm một element con vào sau cùng của một element khác.
+    for (let i=0; i<filterList.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = filterList[i];
+        ul.appendChild(li); // Thêm một element con vào sau cùng của một element khác.
+    }
+    document.body.appendChild(ul);
 }
 
-document.body.appendChild(ul);
+addTodoList('');
+
+function sayHello() {
+    console.log('Hello, My name is MindX');
+    alert('Hello, My name is MindX');
+}
+
+// const btn = document.getElementById('btn');
+// console.log(btn);
+// btn.onclick = sayHello;
+
+// addEventListener
+const btn = document.getElementById('btn');
+function helloEvent() {
+    console.log('Hello event');
+}
+btn.addEventListener('click', helloEvent);
+btn.addEventListener('click', function(event) {
+    console.log(event.target)
+    console.log('Hello function');
+    const btn = document.getElementById('btn');
+    btn.removeEventListener('click', helloEvent);
+})
+
+
+const text2 = document.getElementById('text2');
+text2.addEventListener('change', function(event) {
+    console.log('event.target.value', event.target.value);
+})
+
+// HTML Web Storage
+localStorage.setItem('total', 199000);
+console.log(localStorage.getItem('total'));
+
+sessionStorage.setItem('total', 199000);
+console.log(sessionStorage.getItem('total'));
+
+let data = {
+    name: 'MindX'
+}
+localStorage.setItem('data', JSON.stringify(data));
+console.log(localStorage.getItem('data'));
+console.log(JSON.parse(localStorage.getItem('data')));
+
+
+// Windown localion & History
+console.log(location.href)
+console.log(location.host)
+console.log(location.hostname)
+console.log(location.pathname)
+// location.assign('https://www.google.com/maps')
+
+// history.back();
+// history.forward();
+
+// Timer
+setTimeout(function() {
+    console.log('Set time out');
+}, 3000) // function, time: miliseconds
+
+let i = 5;
+const interval = setInterval(function() {
+    i--;
+    console.log('Set interval', i);
+
+    if (i == 0) {
+        clearInterval(interval);   
+    }
+}, 1000)
+
+
+console.log('End of code');
